4daf680b95dbd8e59bcb7b6c2395e4fb2e8e70a1
Paolo Dona <paolo.dona@gmail.com>
2008-10-27 16:21:46 +0000

    added progressbar helper

---
+++ lib/widgets/progressbar_helper.rb
@@ -0,0 +1,39 @@
+module Widgets
+  module ProgressbarHelper  
+    include CssTemplate
+    
+    # show a progressbar
+    #
+    # eg: <%= progressbar 35, :generate_css => true %>
+    # or  <%= progressbar [35,78,15] %>
+    #
+    # options
+    # ===
+    #   :generate_css defaults to false 
+    #   :adjust       defaults to false
+    def progressbar values, options={}
+      raise ArgumentError, "Missing value(s) parameter in progressbar call" unless values
+      raise ArgumentError, "The value parameter has to be a Numeric o Array" unless values.kind_of?(Array) or values.kind_of?(Numeric)
+      if values.kind_of? Numeric # single value
+        total = 100 
+        values = [values]
+      else # Array of values
+        total = values.sum
+      end 
+      
+      html = ""
+      html << default_css if options[:generate_css] == true
+      html << '<div class="progressbar">'          
+      values.dup.each_with_index do |value, index|
+        percentage = options[:adjust] ? (value * 100 / total) : value
+        css_class = "progressbar_color_#{index.modulo(10)}"
+        html << "<div style='width: #{percentage}%;' class='#{css_class}'></div>"
+      end 
+      html << "</div>" 
+    end
+    
+    def css_template_filename
+      'progressbar.css.erb' 
+    end
+  end
+end
