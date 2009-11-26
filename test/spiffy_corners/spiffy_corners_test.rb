14693995dc9439b6feed4fee02737323d92a2c0a
Matt Southerden <matt@localbubble.com>
2009-11-26 16:28:41 +0000

    Updated spiffy_corners_test to Rails 2.2 ActionView

--- test/spiffy_corners/spiffy_corners_test.rb
+++ test/spiffy_corners/spiffy_corners_test.rb
@@ -1,11 +1,9 @@
 #!/usr/bin/env ruby
 require File.dirname(__FILE__) + '/../test_helper'
 
-class SpiffyCornersHelperTest < Test::Unit::TestCase
+class SpiffyCornersHelperTest < ActionView::TestCase
+
   attr_accessor :params
-  include ActionView::Helpers::TagHelper
-  include ActionView::Helpers::TextHelper
-  include ActionView::Helpers::CaptureHelper
   include Widgets::SpiffyCorners::SpiffyCornersHelper
   
   def setup
@@ -15,8 +13,7 @@ class SpiffyCornersHelperTest < Test::Unit::TestCase
   def test_simple_with_css
     expected = load_template('spiffy_corners/simple.html') 
     
-    _erbout = ''
-    spiffy_corners(:generate_css => true) do concat("Ciccio", binding); end
-    assert_equal expected, _erbout
+    spiffy_corners(:generate_css => true) do concat("Ciccio"); end
+    assert_equal expected, output_buffer
   end
 end
