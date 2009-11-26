ba74494336332bf1d85343414653ee2dfd99f6d6
Matt Southerden <matt@localbubble.com>
2009-11-26 13:20:34 +0000

    Correct load paths in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
-$:.unshift(File.dirname(__FILE__) + '/../..')
-$:.unshift(File.dirname(__FILE__) + '/../../lib')
+$:.unshift(File.dirname(__FILE__) + '/..')
+$:.unshift(File.dirname(__FILE__) + '/../lib')
 
 require 'rubygems'
 require 'test/unit'
