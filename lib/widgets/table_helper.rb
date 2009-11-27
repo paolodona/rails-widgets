519c35fa921ce2cfbe68444ab5d697e760a5e4f5
Matt Southerden <matt@localbubble.com>
2009-11-27 15:39:01 +0000

    Crudely split the tableize helper into a class, so it can easily be refactored without polluting the temmplate namespace with methods.

--- lib/widgets/table_helper.rb
+++ lib/widgets/table_helper.rb
@@ -22,56 +22,81 @@ module Widgets
     #         </tr></tbody></table>
     #
     def tableize(collection = nil, opts = {}, &block)
-      collection ||= opts[:collection]
-      raise ArgumentError, 'Missing collection parameter in tableize call' unless collection
-      raise ArgumentError, 'Missing block in tableize call' unless block_given?
-      columns = opts[:cols] || 3
-      raise ArgumentError, 'Tableize columns must be two or more' unless columns > 1
+      table = Tableizer.new(collection, opts, self, &block)
+      table.render
+    end
+    
+    class Tableizer
       
-      @generate_css = opts[:generate_css] || false
-      @name = opts[:name] || :main
-      html = opts[:html] || {} # setup default html options
-      html[:id] ||= @name.to_s.underscore << '_table'
-      html[:class] ||= html[:id]
-      
-      _out = generate_css? ? render_css('table') : ''
-      _out << tag('table', {:id => html[:id], :class => html[:class]}, true) 
-      _out << tag('tbody', nil, true) 
-      _out << tag('tr', nil, true)
-      
-      index = 0
-      size = collection.size
-      empty_cell = content_tag('td', '&nbsp;', :class => 'blank')
-      # add header
-      if (opts[:header]) 
-        _out << content_tag('th', opts[:header])
-        index += 1
-        size += 1
+      def initialize(collection, opts, template, &block)
+        @collection = collection
+        @collection ||= opts[:collection]
+        
+        @columns = opts[:cols] || 3
+        
+        raise ArgumentError, 'Missing collection parameter in tableize call' unless @collection
+        raise ArgumentError, 'Missing block in tableize call' unless block_given?
+        raise ArgumentError, 'Tableize columns must be two or more' unless @columns > 1
+        
+        @opts = opts
+        @template = template
+        @block = block
       end
-      # fill line with items, breaking if needed
-      collection.each do |item|
-        index += 1
-        _out << content_tag('td', capture(item, &block))
+      
+      def method_missing(*args, &block)
+        @template.send(*args, &block)
+      end
+      
+      def render
+        @collection ||= @opts[:collection]
+        
 
-        _out << '</tr>' << tag('tr', nil, true) if ( index.remainder(columns) == 0 and index != size )
-        
-        # should_wrap =  index.remainder(columns) == 0 and index != size
-        # _out << '</tr>' << tag('tr', nil, true) if should_wrap 
-        
-        # prepend every line with an empty cell
-        # if should_wrap && opts[:skip_header_column] == true
-        #   _out << empty_cell 
-        #   index += 1; size += 1
-        # end
+        @generate_css = @opts[:generate_css] || false
+        name = @opts[:name] || :main
+        html = @opts[:html] || {} # setup default html options
+        html[:id] ||= name.to_s.underscore << '_table'
+        html[:class] ||= html[:id]
+
+        _out = @generate_css ? render_css('table') : ''
+        _out << tag('table', {:id => html[:id], :class => html[:class]}, true) 
+        _out << tag('tbody', nil, true) 
+        _out << tag('tr', nil, true)
+
+        index = 0
+        size = @collection.size
+        empty_cell = content_tag('td', '&nbsp;', :class => 'blank')
+        # add header
+        if (@opts[:header]) 
+          _out << content_tag('th', @opts[:header])
+          index += 1
+          size += 1
+        end
+        # fill line with items, breaking if needed
+        @collection.each do |item|
+          index += 1
+          _out << content_tag('td', capture(item, &@block))
+
+          _out << '</tr>' << tag('tr', nil, true) if ( index.remainder(@columns) == 0 and index != size )
+
+          # should_wrap =  index.remainder(@columns) == 0 and index != size
+          # _out << '</tr>' << tag('tr', nil, true) if should_wrap 
+
+          # prepend every line with an empty cell
+          # if should_wrap && opts[:skip_header_column] == true
+          #   _out << empty_cell 
+          #   index += 1; size += 1
+          # end
+        end
+        # fill remaining columns with empty boxes
+        remaining = size.remainder(@columns)
+         (@columns - remaining).times do
+          _out << empty_cell
+        end unless remaining == 0
+        _out << '</tr>' << '</tbody>' << '</table>' 
+        concat(_out)
+        nil # avoid duplication if called with <%= %>
       end
-      # fill remaining columns with empty boxes
-      remaining = size.remainder(columns)
-       (columns - remaining).times do
-        _out << empty_cell
-      end unless remaining == 0
-      _out << '</tr>' << '</tbody>' << '</table>' 
-      concat(_out)
-      nil # avoid duplication if called with <%= %>
+      
     end
     
   end
