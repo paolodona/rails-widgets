3c78f21e8ad38a14685324c136800bb2fd624411
Matt Southerden <matt@localbubble.com>
2009-11-30 11:52:37 +0000

    More refactoring to Tableizer

--- lib/widgets/table_helper.rb
+++ lib/widgets/table_helper.rb
@@ -29,72 +29,90 @@ module Widgets
     class Tableizer
       
       def initialize(collection, opts, template, &block)
+        parse_args(collection, opts, template, &block)
+        
+        raise ArgumentError, 'Missing collection parameter in tableize call' unless @collection
+        raise ArgumentError, 'Missing block in tableize call' unless block_given?
+        raise ArgumentError, 'Tableize columns must be two or more' unless @columns >= 2
+      end
+      
+      def render
+        generate_css
+        generate_html
+        concat(@output_buffer)
+        nil # avoid duplication if called with <%= %>
+      end
+      
+      protected
+      
+      def parse_args(collection, opts, template, &block)
         @collection = collection
         @collection ||= opts[:collection]
         
         @columns = opts[:cols] || 3
         
-        raise ArgumentError, 'Missing collection parameter in tableize call' unless @collection
-        raise ArgumentError, 'Missing block in tableize call' unless block_given?
-        raise ArgumentError, 'Tableize columns must be two or more' unless @columns > 1
-        
         @generate_css = opts[:generate_css] || false
-        
-        @opts = opts
         
         @name = opts[:name] || :main
         @html = opts[:html] || {} # setup default html options
         @html[:id] ||= @name.to_s.underscore << '_table'
         @html[:class] ||= @html[:id]
         @header = opts[:header]
+        @skip_header_column = opts[:skip_header_column]
         
         @template = template
         @block = block
+        
+        @output_buffer = ''
       end
       
-      def method_missing(*args, &block)
-        @template.send(*args, &block)
+      def generate_css
+        @output_buffer << render_css('table') if generate_css?
       end
       
-      def render
-        _out = @generate_css ? render_css('table') : ''
-        _out << tag('table', {:id => @html[:id], :class => @html[:class]}, true) 
-        _out << tag('tbody', nil, true) 
-        _out << tag('tr', nil, true)
+      def generate_html
+        @output_buffer << tag('table', {:id => @html[:id], :class => @html[:class]}, true) 
+        @output_buffer << tag('tbody', nil, true) 
+        @output_buffer << tag('tr', nil, true)
 
         index = 0
         size = @collection.size
         empty_cell = content_tag('td', '&nbsp;', :class => 'blank')
         # add header
         if (@header) 
-          _out << content_tag('th', @header)
+          @output_buffer << content_tag('th', @header)
           index += 1
           size += 1
         end
         # fill line with items, breaking if needed
         @collection.each do |item|
           index += 1
-          _out << content_tag('td', capture(item, &@block))
+          @output_buffer << content_tag('td', capture(item, &@block))
 
           should_wrap = ( index.remainder(@columns) == 0 and index != size )
-          _out << '</tr>' << tag('tr', nil, true) if should_wrap 
+          @output_buffer << '</tr>' << tag('tr', nil, true) if should_wrap 
 
           # prepend every line with an empty cell
-          if should_wrap && @opts[:skip_header_column] == true
-            _out << empty_cell 
+          if should_wrap && @skip_header_column == true
+            @output_buffer << empty_cell 
             index += 1; size += 1
           end
         end
         # fill remaining columns with empty boxes
         remaining = size.remainder(@columns)
          (@columns - remaining).times do
-          _out << empty_cell
+          @output_buffer << empty_cell
         end unless remaining == 0
-        _out << '</tr>' << '</tbody>' << '</table>' 
-        concat(_out)
-        nil # avoid duplication if called with <%= %>
+        @output_buffer << '</tr>' << '</tbody>' << '</table>'
       end
       
+      def generate_css?
+        @generate_css
+      end
+      
+      def method_missing(*args, &block)
+        @template.send(*args, &block)
+      end
     end
     
   end
